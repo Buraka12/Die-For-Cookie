dc8ad1d5111a5c8cdb9cab82a5a329e1bbdd4956
Buraka12 <taciburak44@gmail.com>
2025-08-26 00:09:51 +0300

    Add audio manager autoload and play main music
    Introduces AudioManager as an autoload singleton and sets up the main music track to play on scene ready. Updates scene and script references to support audio playback, including wiring up the audio stream and volume settings.

--- Scripts/audio_manager.gd
+++ Scripts/audio_manager.gd
@@ -7,7 +7,6 @@ var active_music_Stream : AudioStreamPlayer
 @export var clips:Node
 
 func play(audio_name:String , from_position:float = 0.0) -> void:
-	pass
 	active_music_Stream = clips.get_node(audio_name)
 	active_music_Stream.play(from_position)
 	
