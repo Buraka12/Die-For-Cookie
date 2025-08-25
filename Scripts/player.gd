d1543d87d638f8ea711e767c257713f57ea6443b
Buraka12 <taciburak44@gmail.com>
2025-08-25 21:49:30 +0300

    jump fixes and camera smooth activated
    Renamed the jump animation from 'jumps' to 'jump' in both scene and script for consistency. Improved animation logic in player.gd to handle idle, move, and jump states more clearly. Enabled position smoothing for Camera2D to provide smoother camera movement.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,8 +11,6 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	if not is_on_floor():
-		$AnimatedSprite2D.play("jumps")
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
@@ -22,14 +20,22 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = false
 	elif direction < 0:
 		$AnimatedSprite2D.flip_h = true
-	
+
 	#moving
-	if direction and is_on_floor():
-		$AnimatedSprite2D.play("move")
+	if direction:
 		velocity.x = direction * SPEED
 		
-	elif not direction and is_on_floor():
-		$AnimatedSprite2D.play("idle")
+	elif not direction:
 		velocity.x = move_toward(velocity.x, 0, 12)
-
+		
+		
+	#animations
+	if is_on_floor():
+		if direction == 0:
+			$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("move")	
+	else:
+		$AnimatedSprite2D.play("jump")
+		
 	move_and_slide()
