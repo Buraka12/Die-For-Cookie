90627fd6acb7c3f5df9f277d0121ac9a2e973fc4
Buraka12 <taciburak44@gmail.com>
2025-08-25 21:29:29 +0300

    Add jump animation and improve player movement
    Expanded player.tscn with new AtlasTextures and updated SpriteFrames to include a 'jumps' animation. Modified player.gd to trigger the jump animation when airborne and refined movement logic for better animation transitions. Updated project.godot with a canvas texture filter setting.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
 const SPEED = 200
 const JUMP_VELOCITY = -300
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,9 +11,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if not is_on_floor():
+		$AnimatedSprite2D.play("jumps")
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	
 	#flip sprite
@@ -24,10 +23,12 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 		$AnimatedSprite2D.flip_h = true
 	
-	if direction:
+	#moving
+	if direction and is_on_floor():
 		$AnimatedSprite2D.play("move")
 		velocity.x = direction * SPEED
-	else:
+		
+	elif not direction and is_on_floor():
 		$AnimatedSprite2D.play("idle")
 		velocity.x = move_toward(velocity.x, 0, 12)
 
