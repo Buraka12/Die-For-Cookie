47e46e5ced4203545385d3e89aeb83b3f737bb4f
Metehan <mcgamer0700@gmail.com>
2025-08-26 03:10:14 +0300

    Player Fall&Jump

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,14 +3,21 @@ extends CharacterBody2D
 const SPEED = 200
 const JUMP_VELOCITY = -300
 
+enum states {IDLE,RUN,FALL,PUSH}
+var current_state : states
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if current_state != states.FALL:
+			current_state = states.FALL
+			$AnimatedSprite2D.play("fall")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
@@ -23,9 +30,11 @@ func _physics_process(delta: float) -> void:
 
 	#moving
 	if direction:
+		current_state = states.RUN
 		velocity.x = direction * SPEED
 		
-	elif not direction:
+	elif not direction and current_state != states.FALL:
+		current_state = states.IDLE
 		velocity.x = move_toward(velocity.x, 0, 12)
 		
 		
@@ -34,8 +43,7 @@ func _physics_process(delta: float) -> void:
 		if direction == 0:
 			$AnimatedSprite2D.play("idle")
 		else:
-			$AnimatedSprite2D.play("move")	
-	else:
-		$AnimatedSprite2D.play("jump")
-		
+			$AnimatedSprite2D.play("move")
+	
+	
 	move_and_slide()
