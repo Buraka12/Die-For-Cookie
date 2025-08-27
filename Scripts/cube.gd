2233fb2ebb4276760d33d2f24f47a7abd892f173
Metehan <mcgamer0700@gmail.com>
2025-08-28 02:19:45 +0300

    Some Cube Bug Fixes

--- Scripts/cube.gd
+++ Scripts/cube.gd
@@ -4,12 +4,13 @@ var is_player_near = false
 var push_speed = 70
 
 func _on_grab_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and body.grabbed_body == null:
 		body.grabbed_body = $"."
 		is_player_near = true
 
 func _on_grab_area_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and body.grabbed_body == $".":
+		body.current_state = body.states.IDLE
 		body.grabbed_body = null
 		is_player_near = false
 
@@ -17,5 +18,6 @@ func push(direction:float,extra_speed):
 	if is_player_near:
 		print("İt")
 		linear_velocity.x = direction * (push_speed+extra_speed)
+		$GrabArea.global_rotation = 0
 	else:
 		linear_velocity.x = 0
